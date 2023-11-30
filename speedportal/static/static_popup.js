c0677d4bd51c594717fdddcf613bd2aab71b9c82
Karkusha23 <min.rinat567321@gmail.com>
2023-11-30 20:49:36 +0300

    Add profile popup

--- speedportal/static/static_popup.js
+++ speedportal/static/static_popup.js
@@ -1,4 +1,6 @@
 let staticPopupElem;
+let x_left_border;
+let y_bottom_border;
 
 document.addEventListener('mouseover', e => {
     if (staticPopupElem)
@@ -12,18 +14,30 @@ document.addEventListener('mouseover', e => {
         return;
     }
     staticPopupElem = document.createElement('div');
-    staticPopupElem.setAttribute('onmouseout', 'hid_static_popup()');
     staticPopupElem.className = 'static_popup';
     staticPopupElem.innerHTML = popupHTML;
     document.body.append(staticPopupElem);
 
     let vec = target.getBoundingClientRect();
-    let left = vec.left;
-    let top = vec.top + staticPopupElem.offsetHeight;
+    let left = vec.left - staticPopupElem.offsetWidth + target.offsetWidth;
+    let top = vec.top + target.offsetHeight + 5;
 
-    console.log(vec.left);
     staticPopupElem.style.left = left + 'px';
     staticPopupElem.style.top = top + 'px';
+
+    x_left_border = left;
+    y_bottom_border = top + staticPopupElem.offsetHeight;
+});
+
+document.addEventListener('mousemove', e => {
+    if (!staticPopupElem || !x_left_border || !y_bottom_border)
+    {
+        return;
+    }
+    if (e.x < x_left_border || e.y > y_bottom_border)
+    {
+        hid_static_popup();
+    }
 });
 
 function hid_static_popup()
@@ -32,5 +46,7 @@ function hid_static_popup()
     {
         staticPopupElem.remove();
         staticPopupElem = null;
+        x_left_border = null;
+        y_bottom_border = null;
     }
 }
